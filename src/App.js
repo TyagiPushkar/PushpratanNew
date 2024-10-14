084f2cee160ef6eed1002bafd4291718515eb568
yashxjain <jyash5332@gmail.com>
2024-10-14 14:16:51 +0530

    new

---
+++ src/App.js
@@ -0,0 +1,58 @@
+import React, { useEffect } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { ThemeProvider } from '@mui/material/styles';
+import { AuthProvider } from './components/auth/AuthContext';
+
+import Login from './pages/Login';
+import Dashboard from './pages/Dashboard';
+import theme from './styles/theme';
+import Employee from './pages/Employee';
+import Holiday from './pages/Holiday';
+import Policy from './pages/Policy';
+import Attendance from './pages/Attendance';
+import Notification from './pages/Notification';
+import Leave from './pages/Leave';
+import Expense from './pages/Expense';
+import EmpProfile from './pages/User';
+import PrivateRoute from './components/auth/PrivateRoute';
+
+import EmployeeProfile from './pages/EmployeeProfile';
+import Visit from './pages/Dealer';
+
+function App() {
+   useEffect(() => {
+        const handleRightClick = (event) => {
+            event.preventDefault();
+        };
+
+        document.addEventListener('contextmenu', handleRightClick);
+
+        return () => {
+            document.removeEventListener('contextmenu', handleRightClick);
+        };
+    }, []);
+  return (
+    <ThemeProvider theme={theme}>
+      <AuthProvider>
+        <Router>
+          <Routes>
+            <Route path="/" element={<Login />} />
+            <Route path="/dashboard" element={<PrivateRoute element={Dashboard} />} />
+            <Route path="/employees" element={<PrivateRoute element={Employee} requiredRole="HR" />} />
+            <Route path="/employees/:empId" element={<PrivateRoute element={EmployeeProfile} requiredRole="HR" />} />
+            <Route path="/holiday" element={<PrivateRoute element={Holiday} />} />
+            <Route path="/policy" element={<PrivateRoute element={Policy} />} />
+            <Route path="/attendance" element={<PrivateRoute element={Attendance} />} />
+            <Route path="/notification" element={<PrivateRoute element={Notification} />} />
+            <Route path="/leave" element={<PrivateRoute element={Leave} />} />
+            <Route path="/expense" element={<PrivateRoute element={Expense} />} />
+            <Route path="/profile" element={<PrivateRoute element={EmpProfile} />} />
+            <Route path="/visit" element={<PrivateRoute element={Visit} />} />
+          </Routes>
+        </Router>
+      </AuthProvider>
+    </ThemeProvider>
+  );
+}
+
+export default App;
---
+++ src/components/Sidebar.js
@@ -0,0 +1,118 @@
+import React from 'react';
+import { useLocation, Link } from 'react-router-dom';
+import { Drawer, List, ListItem, ListItemText, Box, Slide } from '@mui/material';
+import HRSmileLogo from '../assets/HRSmileLogo.png';
+import useMediaQuery from '@mui/material/useMediaQuery';
+import { useTheme } from '@mui/material/styles';
+import { useAuth } from './auth/AuthContext';
+
+function Sidebar({ mobileOpen, onDrawerToggle }) {
+    const location = useLocation();
+    const theme = useTheme();
+    const isMobile = useMediaQuery(theme.breakpoints.down('sm'));
+    const { user } = useAuth();
+
+    const routes = [
+        { path: '/dashboard', name: 'Dashboard' },
+        { path: '/holiday', name: 'Holiday' },
+        { path: '/policy', name: 'Policy' },
+        { path: '/attendance', name: 'Attendance' },
+        { path: '/notification', name: 'Notification' },
+        { path: '/leave', name: 'Leave' },
+        { path: '/expense', name: 'Expense' },
+        { path: '/visit', name: 'Visit' },
+    ];
+
+    // Conditionally include the "Employees" tab based on the user's role
+    if (user && user.role === 'HR') {
+        routes.splice(1, 0, { path: '/employees', name: 'Employees' });
+    }
+
+    const drawer = (
+        <Box
+            sx={{
+                width: 240,
+                display: 'flex',
+                flexDirection: 'column',
+                height: '100vh',
+                bgcolor: '#084606',
+                overflow: 'hidden',
+            }}
+        >
+            {/* Fixed logo area */}
+            <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', p: 2 }}>
+                <img src={HRSmileLogo} alt="HRMS Logo" style={{ width: '250px', marginBottom: '20px' }} />
+            </Box>
+
+            {/* Scrollable list area without visible scrollbar */}
+            <Box sx={{ overflowX: 'auto', flex: 1, '&::-webkit-scrollbar': { display: 'none' },marginTop:'80px' }}>
+                <List>
+                    {routes.map((route, index) => (
+                        <ListItem
+                            button
+                            key={index}
+                            component={Link}
+                            to={route.path}
+                            sx={{
+                                backgroundColor: location.pathname === route.path ? 'white' : 'transparent',
+                                color: location.pathname === route.path ? '#084606' : 'white',
+                                '&:hover': {
+                                    backgroundColor: '#6CF851',
+                                    color: 'white',
+                                },
+                                transition: 'background-color 0.3s ease, color 0.3s ease',
+                                borderRadius: '10px',
+                            }}
+                            onClick={isMobile ? onDrawerToggle : null}
+                        >
+                            <ListItemText primary={route.name} />
+                        </ListItem>
+                    ))}
+                </List>
+            </Box>
+        </Box>
+    );
+
+    return (
+        <Box sx={{ display: 'flex' }}>
+            <Slide direction="left" in={mobileOpen} mountOnEnter unmountOnExit>
+                <Drawer
+                    variant="temporary"
+                    open={mobileOpen}
+                    onClose={onDrawerToggle}
+                    ModalProps={{
+                        keepMounted: true,
+                    }}
+                    sx={{
+                        '& .MuiDrawer-paper': {
+                            boxSizing: 'border-box',
+                            width: 240,
+                            zIndex: theme.zIndex.appBar + 1,
+                            backgroundColor: '#084606',
+                            overflowY: 'hidden',
+                        },
+                    }}
+                >
+                    {drawer}
+                </Drawer>
+            </Slide>
+            <Drawer
+                variant="permanent"
+                sx={{
+                    display: { xs: 'none', sm: 'block' },
+                    '& .MuiDrawer-paper': {
+                        boxSizing: 'border-box',
+                        width: 240,
+                        backgroundColor: '#084606',
+                        overflowY: 'hidden',
+                    },
+                }}
+                open
+            >
+                {drawer}
+            </Drawer>
+        </Box>
+    );
+}
+
+export default Sidebar;
